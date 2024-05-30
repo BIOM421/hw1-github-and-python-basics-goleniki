03ebc632f3e9fceecaa673585a9cb2d3afc66b79
goleniki <73141921+goleniki@users.noreply.github.com>
2024-05-31 00:06:34 +0300

    CORRECTION

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 def hello_world():
     return "Hello World!"
     
-hello_world()
 
 def hello_world_n(N):
-    for _ in range(N):   
-        return "Hello World!"
+    tmp = "Hello World! "
+    return (tmp*N)[:-1]

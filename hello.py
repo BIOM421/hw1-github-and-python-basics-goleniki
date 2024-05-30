728ca48db8b4056c0d0ab618ef4e403373439697
goleniki <73141921+goleniki@users.noreply.github.com>
2024-05-30 23:10:21 +0300

    3

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 def hello_world():
-    return 'Hello World!'
+    return "Hello World!"
     
-    
+hello_world()
+
 def hello_world_n(N):
     for _ in range(N):   
-        return 'Hello World!'
+        return "Hello World!"
